e18af4ef05229466a538b94e58c0eb872651527d
MILON-2377 <153436745+MILON-2377@users.noreply.github.com>
2024-02-28 21:29:11 +0800

    make the dynamic the gallery btn and about us

--- script/silkworm.js
+++ script/silkworm.js
@@ -29,3 +29,16 @@ function carosoleSlightImg() {
 }
 
 carosoleSlightImg();
+
+function scrollIntoViewById(idName1, idName2) {
+    const galleryContainer = document.getElementById(idName2);
+    const clickSection = document.getElementById(idName1);
+
+    clickSection.addEventListener("click", () => {
+        galleryContainer.scrollIntoView("{behavior: 'smooth'}");
+    }, false)
+}
+
+scrollIntoViewById("galler-btn", "gallery-container");
+scrollIntoViewById("about-us-btn", "about-us-container");
+
